03ea3f799b24ea6916c28dd76a2cd4648b487559
Class_Student <Class_Student@COPM-4-6-20>
2024-12-27 14:50:14 +0300

    miya

---
+++ demochka/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace demochka
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AddClient_OnClick(object? sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
